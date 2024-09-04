f61e3c06ad045727679369c3c76ba0a0644df125
maxxi <161537671+maxiitietze@users.noreply.github.com>
2024-09-04 18:27:09 -0300

    Delete Resoluciones directory

--- Resoluciones/Guia 1/ejercicio3.hs
+++
@@ -1,6 +0,0 @@
-esMultiploDe :: Integer->Integer->Bool
-esMultiploDe x y = x `mod` y == 0
-
-estanRelacionados :: Integer->Integer->Bool
-estanRelacionados x y | esMultiploDe x y  = True
-                       | otherwise = False
