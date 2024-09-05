00f74a3ec94cfe506854630b12fa0424d68cae0f
maxxi <161537671+maxiitietze@users.noreply.github.com>
2024-09-05 13:53:42 -0300

    Delete Guia 3 directory

--- Guia 3/ejercicio6.hs
+++
@@ -1,6 +0,0 @@
-type Anio = Integer
-type EsBisiesto = Bool
-
-bisiesto :: Anio->EsBisiesto
-bisiesto x | (x `mod `4 /= 0) || (x `mod `100 == 0) && (x `mod `400 /= 0) = False
-            | otherwise = True
